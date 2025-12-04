39db6f8abc46c823bc9dd19d78e89a77203e600c
ZhouZhenYun <2922188469@qq.com>
2025-12-04 15:23:55 +0800

    build(prisma): 配置 next.js 支持 prisma client wasm 打包

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+  // 让 Next.js 支持打包 Prisma Cloudflare Client 使用的 WASM（query_engine_bg.wasm?module）
+  webpack(config) {
+    config.experiments = {
+      ...(config.experiments || {}),
+      asyncWebAssembly: true,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
